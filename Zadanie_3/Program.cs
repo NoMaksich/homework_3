7ae8613b96132702f87e2cbb97779a9c40d66ca9
maximmetalnikov <maxummetalnikov@mail.ru>
2023-04-28 21:02:38 +0300

    Третье задание выполнено

---
+++ Zadanie_3/Program.cs
@@ -0,0 +1,29 @@
+﻿int n= Prompt("Введите число больше 1: ");
+if (n>=1)
+{
+    Table(n);
+}
+else
+{
+    Console.WriteLine("Число меньше 1!");
+}
+
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value=int.Parse(Console.ReadLine());
+    int numN=Convert.ToInt32(value);
+    return numN;
+}
+
+void Table(int number)
+{
+    int startNumber=1;
+    while (startNumber<=number)
+    {
+        Console.Write(startNumber*startNumber*startNumber+ " ");
+        startNumber++;
+    }
+}
