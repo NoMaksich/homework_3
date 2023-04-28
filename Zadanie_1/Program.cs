ab195014e8422db442b9303c864dab1071ad06c9
maximmetalnikov <maxummetalnikov@mail.ru>
2023-04-28 21:02:20 +0300

    Первое задание выполнено

---
+++ Zadanie_1/Program.cs
@@ -0,0 +1,42 @@
+﻿int n= Prompt("Введите пятизначное число: ");
+if (n>=10000 && n<=99999)
+{
+    Palindrom(n);
+}
+else
+{
+    Console.WriteLine("Вы ввели не пятизначное число!");
+}
+
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value=int.Parse(Console.ReadLine());
+    int numN=Convert.ToInt32(value);
+    return numN;
+}
+
+void Palindrom(int number)
+{
+    int numberOne=number/10000;
+    int numberFive=number%10;
+    if (numberOne==numberFive)
+    {
+        int numberTwo=(number/1000)%10;
+        int numberFour=(number%100)/10;
+        if(numberTwo==numberFour)
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
